efceba6543eaf357113bb0f0af062d29f88025f6
NickiHell <nickihell@gmail.com>
2021-08-16 00:59:18 +1000

    Refactoring

--- app/bots/classes/bots.py
+++ app/bots/classes/bots.py
@@ -45,7 +45,7 @@ class DumdBot:
 
     async def _morning(self):
         while True:
-            await asyncio.sleep(30)
+            await asyncio.sleep(45)
             morning = datetime.now().replace(hour=9)
             if abs(datetime.now() - morning) < timedelta(minutes=1):
                 chat_id: int = -1001304348662
