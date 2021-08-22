4913e4ea3a80bd32d3dba6a2c23fd51432cd832b
Nicki Hell <nickihell@gmail.com>
2021-08-22 03:02:18 +0000

    Tests CI

--- .space.kts
+++ .space.kts
@@ -2,9 +2,9 @@ job("Tests") {
     container(displayName = "Python Cotainer", image = "python:3.9.6-bullseye") {
     shellScript {
             content = """
-                ls
-                apt update
-                apt install tree
+                curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+                $HOME/.poetry/bin/poetry shell
+                task tests
             """
         }
 }}
