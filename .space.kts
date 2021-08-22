03db6b4ccdcfac5c10d920fdfaf74e4b78d70d04
Nicki Hell <nickihell@gmail.com>
2021-08-22 11:18:11 +0000

    fsdfdsfd

--- .space.kts
+++ .space.kts
@@ -1,31 +1,74 @@
 job("QA and Linters") {
-    container(displayName = "Tests", image = "python:3.9.6-bullseye") {
-    			
-    shellScript {
-            content = """
-                curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-                /root/.poetry/bin/poetry install
-                /root/.poetry/bin/poetry run task pytest
-            """
-        }
+    parallel {
+        container(displayName = "Tests", image = "python:3.9.6-bullseye") {		
+            shellScript {
+                    content = """
+                        curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+                        /root/.poetry/bin/poetry install
+                        /root/.poetry/bin/poetry run task pytest
+                    """
+                }
+            }
+            container(displayName = "Mypy", image = "python:3.9.6-bullseye") {
+            shellScript {
+                    interpreter = "/bin/bash"
+                    content = """
+                        curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+                        /root/.poetry/bin/poetry install
+                        /root/.poetry/bin/poetry run task mypy
+                    """
+                }
+            }
+            container(displayName = "FlakeHell", image = "python:3.9.6-bullseye") {
+            shellScript {
+                    content = """
+                        curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+                        /root/.poetry/bin/poetry install
+                        /root/.poetry/bin/poetry run task flakehell
+                    """
+                }
+            }
+    	}
 	}
-    container(displayName = "Mypy", image = "python:3.9.6-bullseye") {
-    shellScript {
-        	interpreter = "/bin/bash"
-            content = """
-                curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-                /root/.poetry/bin/poetry install
-                /root/.poetry/bin/poetry run task mypy
-            """
+
+sequential {
+           container(displayName = "Generate data", image = "gradle:jdk11") {
+               kotlinScript { api ->
+                   api.gradle("generateTestData")
+               }
+           }
+           container(displayName = "Run int tests", image = "gradle:jdk11") {
+               kotlinScript { api ->
+                   api.gradle("integrationTests")
+               }
+           }
+       }
+
+job("Build and run tests") {
+    container(displayName = "Run build", image = "gradle:jdk11") {
+        kotlinScript { api ->
+            api.gradle("build", "-x", "test")
         }
-	}
-    container(displayName = "FlakeHell", image = "python:3.9.6-bullseye") {
-    shellScript {
-            content = """
-                curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-                /root/.poetry/bin/poetry install
-                /root/.poetry/bin/poetry run task flakehell
-            """
-        }
-	}
+   }
+
+   parallel {
+       container(displayName = "Run mainTests", image = "gradle:jdk11") {
+           kotlinScript { api ->
+               api.gradle("mainTests")
+           }
+       }
+
+       sequential {
+           container(displayName = "Generate data", image = "gradle:jdk11") {
+               kotlinScript { api ->
+                   api.gradle("generateTestData")
+               }
+           }
+           container(displayName = "Run int tests", image = "gradle:jdk11") {
+               kotlinScript { api ->
+                   api.gradle("integrationTests")
+               }
+           }
+       }
+   }
 }
