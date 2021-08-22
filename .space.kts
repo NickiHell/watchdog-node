6e2af4b37d31d11298c8944d17fee5e873923f39
Nicki Hell <nickihell@gmail.com>
2021-08-22 11:01:57 +0000

    wertrfe

--- .space.kts
+++ .space.kts
@@ -1,19 +1,11 @@
 job("QA and Linters") {
-    container(displayName = "Isort", image = "python:3.9.6-bullseye") {
-    shellScript {
-        	interpreter = "/bin/bash"
-            content = """
-                curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-                /root/.poetry/bin/poetry install
-                /root/.poetry/bin/poetry run task isort
-            """
-        }
-	}
     container(displayName = "Tests", image = "python:3.9.6-bullseye") {
     			
     shellScript {
             content = """
-                ls
+                curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+                /root/.poetry/bin/poetry install
+                /root/.poetry/bin/poetry run task pytest
             """
         }
 	}
@@ -21,14 +13,18 @@ job("QA and Linters") {
     shellScript {
         	interpreter = "/bin/bash"
             content = """
-                ls
+                curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+                /root/.poetry/bin/poetry install
+                /root/.poetry/bin/poetry run task mypy
             """
         }
 	}
     container(displayName = "FlakeHell", image = "python:3.9.6-bullseye") {
     shellScript {
             content = """
-                ls
+                curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+                /root/.poetry/bin/poetry install
+                /root/.poetry/bin/poetry run task flakehell
             """
         }
 	}
