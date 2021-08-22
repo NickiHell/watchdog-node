cca99a44ade429fb6edd855c36fb496c909b836c
Nicki Hell <nickihell@gmail.com>
2021-08-22 02:55:43 +0000

    fdgdf

--- .space.kts
+++ .space.kts
@@ -1,5 +1,5 @@
 job("Tests") {
-    container(displayName = "Python Cotainer", image = "3.9.6-bullseye") {
+    container(displayName = "Python Cotainer", image = "python:3.9.6-bullseye") {
     shellScript {
             content = """
                 ls
