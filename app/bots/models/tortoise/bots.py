4b1247b738486874acd63b6b081533a4611c73b8
NickiHell <nickihell@gmail.com>
2021-08-22 21:04:22 +1000

    Fixes

--- app/bots/models/tortoise/bots.py
+++ app/bots/models/tortoise/bots.py
@@ -5,6 +5,7 @@ class Bot(Model):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=64)
     token = fields.CharField(max_length=64)
+    net = fields.ForeignKeyField('ml.Net', on_delete=fields.SET_NULL, null=True)
 
     def __dict__(self):
         return {
--- app/ml/models/tortoise/ml.py
+++ app/ml/models/tortoise/ml.py
@@ -5,6 +5,7 @@ class Net(Model):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=64)
     path = fields.CharField(max_length=128)
+    params = fields.JSONField()
 
 
 class Dataset(Model):
