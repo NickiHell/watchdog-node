25298ea783f94cebea83aedb6afa56c0c357dca7
NickiHell <nickihell@ya.ru>
2022-10-28 14:45:25 +0300

    build(bump): refactoring

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -13,5 +13,9 @@ readonly cmd="$*"
 dockerize -wait "tcp://${DJANGO_DATABASE_HOST}:${DJANGO_DATABASE_PORT}" -timeout 120s
 >&2 echo 'Postgres is up - continuing...'
 
+python /app/manage.py migrate --noinput
+python /app/manage.py collectstatic --noinput --clear
+python /app/manage.py compilemessages
 
-exec $cmd
+
+exec "$cmd"
