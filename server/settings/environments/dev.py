d4a9175abfe1bde24d52d778302a388273e8ee51
NickiHell <nickihell@ya.ru>
2023-06-25 18:08:46 +0300

    refactor(action): tiny refactor

--- server/settings/environments/dev.py
+++ server/settings/environments/dev.py
@@ -53,10 +53,7 @@ MIDDLEWARE += (
 
 # https://django-debug-toolbar.readthedocs.io/en/stable/installation.html#configure-internal-ips
 try:  # This might fail on some OS
-    INTERNAL_IPS = [
-        '{}.1'.format(ip[: ip.rfind('.')])
-        for ip in socket.gethostbyname_ex(socket.gethostname())[2]
-    ]
+    INTERNAL_IPS = ['{}.1'.format(ip[: ip.rfind('.')]) for ip in socket.gethostbyname_ex(socket.gethostname())[2]]
 except OSError:  # pragma: no cover
     INTERNAL_IPS = []
 INTERNAL_IPS += ['127.0.0.1', '10.0.2.2']
