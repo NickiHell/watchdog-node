4eba3046b910a169ded62859776713eade771795
NickiHell <nickihell@ya.ru>
2023-10-22 11:19:24 +0300

    refactor(project): update pkgs

--- ros2_ws/src/core/setup.py
+++ ros2_ws/src/core/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'watchdog_node_001 = core.watchdog_node_001:main'
+            'core = core.core:main'
         ],
     },
 )
--- ros2_ws/src/robot/setup.py
+++ ros2_ws/src/robot/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'watchdog_node_001 = robot.watchdog_node_001:main'
+            'node = robot.node:main'
         ],
     },
 )
